e634634b3030aa70e5ef86c81affa6734aa98303
JeissonRincon <32280112+JeissonRincon@users.noreply.github.com>
2020-06-06 21:34:24 -0500

    Add files via upload

---
+++ Entrega4/InterfazGraficaJava/InterfazGraficaJava/src/app/App.java
@@ -0,0 +1,17 @@
+package app;
+
+//@author Jeisson
+
+import app.cliente.VistaPrincipalTemplate;
+import app.cliente.LoginTemplate;
+
+public class App {
+
+    public static void main(String[] args) throws Exception{
+        //VistaPrincipalTemplate vista= new VistaPrincipalTemplate();
+        LoginTemplate login= new LoginTemplate();
+        login.setName("Login");
+        
+    }
+    
+}
